f4d1e603cc6ad675f34fdabc400ff41d448cc679
Ali Shirani <alishiranii1384@gmail.com>
2023-04-23 16:58:09 +0330

    test 3

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -5,7 +5,7 @@ function Avatar({user}:{user:string|undefined|null}) {
   
   return (
     <div className="avatar online">
-      <div className="w-12 h-12 rounded-full bg-neutral">
+      <div className="w-12 h-12 rounded-full bg-neutral flex items-center justify-center">
         <span className="text-3xl uppercase dark:text-white font-bold text-black">{user}</span>
       </div>
     </div>
