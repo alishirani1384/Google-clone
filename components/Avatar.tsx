b0d5f484f63fc372f317290a53148a5731966604
Ali Shirani <alishiranii1384@gmail.com>
2023-04-23 17:38:33 +0330

    fixed

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Avatar({user}:{user:string|undefined|null}) {
   
   return (
-    <div className="bg-white rounded-full dark:bg-neutral online placeholder">
+    <div className="avatar !bg-white rounded-full dark:bg-neutral online placeholder">
       <div className="w-12 h-12 rounded-full bg-neutral flex items-center justify-center">
         <span className="text-3xl uppercase dark:text-white font-bold text-black">{user}</span>
       </div>
