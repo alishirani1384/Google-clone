87412aabcd4caec132f897c5934dd8588509e3e5
Ali Shirani <alishiranii1384@gmail.com>
2023-04-30 00:58:14 +0330

    test

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,17 +1,14 @@
 "use client"
+
 import React from 'react'
 
 function Pagination() {
-
-   function handleClick(){
-        
-    }
   return (
     <div className="btn-group grid grid-cols-2 mt-16 max-w-xl">
       <button className="btn btn-outline dark:text-white hover:border-none hover:bg-violet-500">
         Previous page
       </button>
-      <button onClick={handleClick} className="btn btn-outline dark:text-white hover:border-none hover:bg-violet-500">
+      <button className="btn btn-outline dark:text-white hover:border-none hover:bg-violet-500">
         Next
       </button>
     </div>
