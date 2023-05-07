e9e7d7bb2b05a221f14637343e0d3d28bdd7b092
Ali Shirani <alishiranii1384@gmail.com>
2023-05-07 18:58:51 +0330

    added another api

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -15,40 +15,48 @@ export async function generateMetadata({
   };
 }
 
-async function page({ searchParams }: { searchParams: { q: string; page:string} }) {
-  const url = `https://google-search72.p.rapidapi.com/search?query=${searchParams.q}`;
-  const options = {
-    method: "GET",
-    headers: {
-      "content-type": "application/octet-stream",
-      "X-RapidAPI-Key": "436a2fee38msh16b72c7bf3ef475p1ca5e8jsn868f2489df15",
-      "X-RapidAPI-Host": "google-search72.p.rapidapi.com",
-    },
-  };
-  const response = await fetch(url, options);
+async function page({
+  searchParams,
+}: {
+  searchParams: { q: string; page: string };
+}) {
+  const url = `https://www.googleapis.com/customsearch/v1?q=${searchParams.q}&key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_CX}&alt=json`;
+  const response = await fetch(url);
   const data = await response.json();
- console.log(data);
- 
+
   return (
     <div className="dark:text-white container flex flex-col px-4 py-6 mx-auto">
       <p className="text-sm font-semibold">
-       Page {+searchParams.page + 1} of About {data.estimatedResultCount} results
+        About {data.searchInformation.formattedTotalResults} results (
+        {data.searchInformation.formattedSearchTime} seconds)
       </p>
       <div className="flex flex-col mt-5 gap-5">
-        {data.items?.map((s: { link: string | undefined; title: string | undefined; displayLink: string | undefined; snippet: string | undefined; }, i: React.Key | null | undefined) => {
-          return (
-            <div key={i}>
-              <h2 className="text-lg font-semibold"><a href={s.link} target="_block">{s.title}</a></h2>
-              <small className="dark:text-gray-300">{s.displayLink}</small>
-              <p className="text-sm">{s.snippet}</p>
-            </div>
-          );
-        })}
-        {
-          data.message && <h3>{data.message}</h3>
-        }
+        {data.items?.map(
+          (
+            s: {
+              link: string | undefined;
+              title: string | undefined;
+              displayLink: string | undefined;
+              snippet: string | undefined;
+            },
+            i: React.Key | null | undefined
+          ) => {
+            return (
+              <div key={i}>
+                <h2 className="text-lg font-semibold">
+                  <a href={s.link} target="_block">
+                    {s.title}
+                  </a>
+                </h2>
+                <small className="dark:text-gray-300">{s.displayLink}</small>
+                <p className="text-sm">{s.snippet}</p>
+              </div>
+            );
+          }
+        )}
+        {data.message && <h3>{data.message}</h3>}
       </div>
-      <Pagination/>
+      <Pagination />
     </div>
   );
 }
--- store/search.ts
+++ store/search.ts
@@ -4,8 +4,19 @@ interface SearchState {
   search: string;
   setSearch: (value:string) => void;
 }
+interface PaginationState {
+  page: number;
+  nextPage: (value:number) => void;
+  prevPage: (value:number) => void;
+}
 
 export const useSearch = create<SearchState>()((set) => ({
   search: "",
   setSearch: (value) => set(() => ({ search: value })),
 }));
+
+export const usePagination = create<PaginationState>()((set) => ({
+  page: 0,
+  nextPage: (value) => set(() => ({ page: value })),
+  prevPage: (value) => set(() => ({ page: value })),
+}))
