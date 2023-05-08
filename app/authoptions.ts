321b5e2b35f0fbd43a19f35f4f52184fe9b48eb5
Ali Shirani <alishiranii1384@gmail.com>
2023-05-08 16:51:29 +0330

    fixed api issues

--- app/authoptions.ts
+++ app/authoptions.ts
@@ -3,11 +3,16 @@ import type { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
   providers: [
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  }
 };
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -20,36 +20,82 @@ async function page({
 }: {
   searchParams: { q: string; page: string };
 }) {
-  const url = `https://www.googleapis.com/customsearch/v1?q=${searchParams.q}&key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_CX}&alt=json`;
-  const response = await fetch(url);
+  const url = `https://g-search.p.rapidapi.com/search?q=${searchParams.q}&num=12`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "436a2fee38msh16b72c7bf3ef475p1ca5e8jsn868f2489df15",
+      "X-RapidAPI-Host": "g-search.p.rapidapi.com",
+    },
+  };
+  const response = await fetch(url, options);
   const data = await response.json();
 
   return (
     <div className="dark:text-white container flex flex-col px-4 py-6 mx-auto">
-      <p className="text-sm font-semibold">
-        About {data.searchInformation?.formattedTotalResults} results (
-        {data.searchInformation?.formattedSearchTime} seconds)
-      </p>
+      <p className="text-sm font-semibold">{data.data.result_stat}</p>
+      <div className="flex flex-col gap-3 my-10 xl:w-2/3">
+        <b className="text-lg">{data.data.knowledge_graph.description}</b>
+        <ul className="hidden md:flex gap-8">
+          {data.data.knowledge_graph.informations?.map(
+            (
+              d: {
+                value: {
+                  title:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<
+                        any,
+                        string | React.JSXElementConstructor<any>
+                      >
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                  desc:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<
+                        any,
+                        string | React.JSXElementConstructor<any>
+                      >
+                    | React.ReactFragment
+                    | React.ReactPortal
+                    | null
+                    | undefined;
+                };
+              },
+              i: React.Key | null | undefined
+            ) => (
+              <li key={i} className="flex flex-col text-sm">
+                {d.value.title} : <b>{d.value.desc}</b>
+              </li>
+            )
+          )}
+        </ul>
+      </div>
+
       <div className="flex flex-col mt-5 gap-5">
-        {data.items?.map(
+        {data.data.organic_results?.map(
           (
             s: {
-              link: string | undefined;
+              url: string | undefined;
               title: string | undefined;
-              displayLink: string | undefined;
-              snippet: string | undefined;
+              desc: string | undefined;
             },
             i: React.Key | null | undefined
           ) => {
             return (
               <div key={i}>
                 <h2 className="text-lg font-semibold">
-                  <a href={s.link} target="_block">
+                  <a href={s.url} target="_block">
                     {s.title}
                   </a>
                 </h2>
-                <small className="dark:text-gray-300">{s.displayLink}</small>
-                <p className="text-sm">{s.snippet}</p>
+                <small className="dark:text-gray-300">{s.url}</small>
+                <p className="text-sm">{s.desc}</p>
               </div>
             );
           }
