0452cc5b5a8fbc382dc62996cccb6f0c39355885
Ali Shirani <alishiranii1384@gmail.com>
2023-05-11 20:43:11 +0330

    added image gallery

--- app/search/images/page.tsx
+++ app/search/images/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 async function page({
@@ -10,7 +11,33 @@ async function page({
   const data = await response.json();
   console.log(data);
 
-  return <div>page</div>;
+  return (
+    <div className="container mx-auto grid grid-cols-2 lg:grid-cols-5 gap-4 box-border">
+      {data.items?.map(
+        (
+          item: {
+            image: {
+              thumbnailLink: string;
+              height: number;
+              width: number;
+            };
+            title: string;
+          },
+          i: React.Key | null | undefined
+        ) => (
+          <div key={i} className="shadow-lg h-fit whitespace-pre-wrap">
+            <Image
+              src={item.image.thumbnailLink}
+              alt={item.title}
+              width={400}
+              height={250}
+              className="object-cover rounded-lg"
+            />
+          </div>
+        )
+      )}
+    </div>
+  );
 }
 
 export default page;
--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com"],
   },
-}
+};
 
 module.exports = nextConfig
