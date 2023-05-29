87d2c7d99d4b288d37dd4e92147051cf1b72399d
Ali Shirani <alishiranii1384@gmail.com>
2023-05-29 23:38:03 +0330

    added image gallery and loading page for that

--- app/search/images/page.tsx
+++ app/search/images/page.tsx
@@ -9,33 +9,34 @@ async function page({
   const url = `https://www.googleapis.com/customsearch/v1?fields=searchInformation,queries,items&key=${process.env.GOOGLE_KEY}&cx=${process.env.GOOGLE_CX}&q=${searchParams.q}&start=${searchParams.start}&searchType=image`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
 
   return (
-    <div className="container mx-auto grid grid-cols-2 lg:grid-cols-5 gap-4 box-border">
-      {data.items?.map(
-        (
-          item: {
-            image: {
-              thumbnailLink: string;
-              height: number;
-              width: number;
-            };
-            title: string;
-          },
-          i: React.Key | null | undefined
-        ) => (
-          <div key={i} className="shadow-lg h-fit whitespace-pre-wrap">
-            <Image
-              src={item.image.thumbnailLink}
-              alt={item.title}
-              width={400}
-              height={250}
-              className="object-cover rounded-lg"
-            />
-          </div>
-        )
-      )}
+    <div className="container mx-auto grid gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-1 gap-4">
+        {data.items?.map(
+          (
+            item: {
+              image: {
+                thumbnailLink: string;
+                height: number;
+                width: number;
+              };
+              title: string;
+            },
+            i: React.Key | null | undefined
+          ) => (
+            <div key={i}>
+              <Image
+                className="h-full max-w-full rounded-lg object-cover"
+                src={item.image.thumbnailLink}
+                width={item.image.width}
+                height={item.image.height}
+                alt=""
+              />
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
